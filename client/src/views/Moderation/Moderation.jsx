4b0b6c382eb53bb9332910d57a715b5961397d60
lim-lucas <93298664+lim-lucas@users.noreply.github.com>
2023-11-14 22:02:32 -0500

    Add files via upload

--- client/src/views/Moderation/Moderation.jsx
+++ client/src/views/Moderation/Moderation.jsx
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+ import React, { Component } from 'react';
 import NavBar from '../../components/NavBar/NavBar';
+import './Moderation.less';
 
 class ModerationPage extends Component {
     constructor(props) {
@@ -8,6 +9,14 @@ class ModerationPage extends Component {
             contentData: [
                 { id: 1, title: 'Content 1', description: 'This is the first content item' },
                 { id: 2, title: 'Content 2', description: 'This is the second content item' },
+                { id: 3, title: 'Content 3', description: 'This is the third content item' },
+                { id: 4, title: 'Content 4', description: 'Report Details' },
+                { id: 5, title: 'Content 5', description: 'Report Details' },
+                { id: 6, title: 'Content 6', description: 'Report Details' },
+                { id: 7, title: 'Content 7', description: 'Report Details' },
+                { id: 8, title: 'Content 8', description: 'Report Details' },
+                { id: 9, title: 'Content 9', description: 'Report Details' },
+                
                 // Add more content items here
             ],
             selectedContent: null,
@@ -35,34 +44,51 @@ class ModerationPage extends Component {
         const { contentData, selectedContent } = this.state;
 
         return (
-            <div>
+            <div className='container nav-padding'>
                 <NavBar />
-                 <div id='main-header'>Moderation </div>
-                 <div id='moderation-title'>Moderation</div>
+
+                <div id='moderation-title'>Moderation Page</div> 
+                <div id='moderation-wrapper'>
 
                 {/* Content List */}
-                <ul>
+                <div class = "ContentBox">
+                    
+                <ol class = "ReportGrid">
                     {contentData.map(content => (
+
+                       
+
                         <li key={content.id} onClick={() => this.handleContentClick(content.id)}>
-                            {content.title} - {content.description}
+                             <div class = "ItemBox">
+
+                            <div class = "ItemText">
+                            {content.title}
+                            </div>
+                            
+                            </div>
                         </li>
+
                     ))}
-                </ul>
+                </ol>             
+            
+            </div>
 
-                {/* Moderation Actions */}
-                <div>
-                    <button onClick={() => this.handleModerationAction('approve')}>Approve</button>
-                    <button onClick={() => this.handleModerationAction('reject')}>Reject</button>
-                </div>
-
-                {/* Content Details */}
-                {selectedContent && (
-                    <div>
+            </div>
+        
+            {selectedContent && (
+                    <div class = "ReportBox">
                         <h2>Content Details</h2>
                         <p>Title: {selectedContent.title}</p>
                         <p>Description: {selectedContent.description}</p>
+
+                        <button onClick={() => this.handleModerationAction('approve')} class = "approveButton" >Approve</button>
+                        <button onClick={() => this.handleModerationAction('reject')} class = "rejectButton" >Reject</button>
                     </div>
                 )}
+
+                <br></br>
+                <br></br>
+
             </div>
             
         );
