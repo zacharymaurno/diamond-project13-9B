1346a4dec3d6cc5a7b20b9a6d3532c2ddf89e97f
zacharymaurno <zmaurno@gmail.com>
2023-11-24 10:38:28 -0500

    updated gallery

--- client/src/views/Mentor/Classroom/Gallery/Gallery.jsx
+++ client/src/views/Mentor/Classroom/Gallery/Gallery.jsx
@@ -1,20 +1,43 @@
 import React, { useEffect, useState } from 'react';
-import {
-  deleteStudent,
-  getClassroom,
-  setEnrollmentStatus,
-  updateStudent,
-} from '../../../../Utils/requests';
 import './Gallery.less';
 import MentorSubHeader from '../../../../components/MentorSubHeader/MentorSubHeader';
 import { Form, message } from 'antd';
 import { useNavigate } from 'react-router-dom';
+import { getMentor, getClassroom } from '../../../../Utils/requests';
 
 export default function Gallery({ classroomId }) {
+  const [user, setUser] = useState({});
+  const [data, setData] = useState([]);
+  const [classIds, setClassIds] = useState({});
+  const [classroomDetails, setClassroomDetails] = useState(null);
 
+
+    useEffect(() => {
+        getData();
+    }, []);
+  const navigate = useNavigate();
   const handleBack = () => {
     navigate('/dashboard');
   };
+  async function getData() {
+    try {
+        const userData = await getMentor();
+        console.log(userData);
+        setUser(userData);
+        setData(userData.data.classrooms);
+        console.log(userData.data.classrooms);
+
+        const classIds = userData.data.classrooms.map((classroom) => classroom.id);
+        setClassIds(classIds);
+        const details = await getClassroom(classroomId);
+        setClassroomDetails(details);
+    }
+    catch (error){
+        console.error('Error fetching data: ', error);
+    }
+  }
+   
+  
 
   return (
     <div>
@@ -24,8 +47,23 @@ export default function Gallery({ classroomId }) {
       <MentorSubHeader
         title={'Gallery'}
         classroomId={classroomId}
-        
       />
+        
+            {classroomDetails !== null && (
+                <div id ='card-container'>
+                    {(classroomDetails.data) ? (
+
+                    <div id='card-container'>
+                        {classroomDetails.data.contents.map((content) => (
+                            <li key ={content.id}>Description: {content.description} 
+                            <div>Flag count: {content.flags}</div></li> 
+                        ))}
+                    </div>
+                        ) : (
+                            <div>No content found for this classroom.</div>
+                        )}
+                </div>
+            )}    
       
     </div>
   );
