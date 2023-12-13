53c3a3d52fe494ae6ce392cee3c1d9cdba634fe7
lim-lucas <93298664+lim-lucas@users.noreply.github.com>
2023-12-12 21:29:17 -0500

    Merging all changes

--- client/src/views/Mentor/Classroom/Classroom.jsx
+++ client/src/views/Mentor/Classroom/Classroom.jsx
@@ -7,6 +7,7 @@ import Roster from './Roster/Roster';
 import Home from './Home/Home';
 import SavedWorkSpaceTab from '../../../components/Tabs/SavedWorkspaceTab';
 import { useSearchParams, useParams } from 'react-router-dom';
+import Gallery from './Gallery/Gallery';
 
 const { TabPane } = Tabs;
 
@@ -51,6 +52,11 @@ export default function Classroom({
             classroomId={id}
           />
         </TabPane>
+
+        <TabPane tab='Gallery' key='gallery'>
+          <Gallery handleLogout={handleLogout} classroomId={id} />
+        </TabPane>
+      
       </Tabs>
     </div>
   );
